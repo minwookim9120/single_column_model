553454847481fbf3e772b56aebc255968f030642
minwookim9120 <kdj9120@smail.kongju.ac.kr>
2020-05-12 09:46:49 +0900

    revision dist.

--- for_dmdt/src/main/Mod_integration.f90
+++ for_dmdt/src/main/Mod_integration.f90
@@ -155,36 +155,36 @@ MODULE Mod_integration
       ! temp%dout     = 0. 
       ! q%dout        = 0.
 
-      drop%dout(:,1)=drop%num(:)
-      drop%dout(:,1)=drop%num(:)
-
-      do i = 1, drop_column_num
-      drop%dmb = drop%mb(i+1)-drop%mb(i)
-      enddo
-
-      do it  = 0, nt-1
-        drop%dmb_dt(it) = 4.*pi*rb(:)*1. / (((Rv*T)/(Dv*es)) + (((L/(Rv*T))-1)*(L/(Ka*T)))) *S*dt  !  ; dm@units = "m-2 s-1"
-      end do 
-
-      DO it = 1, nt
-
-        CALL Sub_Finite_volume_PPM ( drop%num, q%sfc_dt(it),  &
-                                     q%top_dt(it),           &
-                                     drop%dmb, drop_column_num, CFL%dz,         &
-                                     1,                        &
-                                     w%stag_dz,                 &
-                                     drop%next_num               &
-                                                               )
-        temp%dz(:)=temp%next_dz(:)
-        q%dz(:)=q%next_dz(:)
-
-        temp%dout(:,it+1)=temp%next_dz(:)
-        q%dout(:,it+1)=q%next_dz(:)
-
-        ! CALL Sub_Cal_P
-        ! CALL Sub_Cal_W
-        !!CALL cloud_pysics
-      ENDDO !! time do
+      ! drop%dout(:,1)=drop%num(:)
+      ! drop%dout(:,1)=drop%num(:)
+      !
+      ! do i = 1, drop_column_num
+      ! drop%dmb = drop%mb(i+1)-drop%mb(i)
+      ! enddo
+      !
+      ! do it  = 0, nt-1
+      !   drop%dmb_dt(it) = 4.*pi*rb(:)*1. / (((Rv*T)/(Dv*es)) + (((L/(Rv*T))-1)*(L/(Ka*T)))) *S*dt  !  ; dm@units = "m-2 s-1"
+      ! end do 
+      !
+      ! DO it = 1, nt
+      !
+      !   CALL Sub_Finite_volume_PPM ( drop%num, q%sfc_dt(it),  &
+      !                                q%top_dt(it),           &
+      !                                drop%dmb, drop_column_num, CFL%dz,         &
+      !                                1,                        &
+      !                                w%stag_dz,                 &
+      !                                drop%next_num               &
+      !                                                          )
+      !   temp%dz(:)=temp%next_dz(:)
+      !   q%dz(:)=q%next_dz(:)
+      !
+      !   temp%dout(:,it+1)=temp%next_dz(:)
+      !   q%dout(:,it+1)=q%next_dz(:)
+      !
+      !   ! CALL Sub_Cal_P
+      !   ! CALL Sub_Cal_W
+      !   !!CALL cloud_pysics
+      ! ENDDO !! time do
 
     END SUBROUTINE Sub_Integration_PPM_dist
 
