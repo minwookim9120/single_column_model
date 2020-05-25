2f4c7893a8e0b63ecfc71a8f314a91d05b2f1523
minwookim9120 <kdj9120@smail.kongju.ac.kr>
2020-05-25 15:49:33 +0900

    update terminal v.

--- src/phys/Mod_drop_growth.f90
+++ src/phys/Mod_drop_growth.f90
@@ -89,7 +89,7 @@ MODULE Mod_drop_growth
                              X_rg, Y_rg, N_Re,  &
                              sigma, d0_max, Bo, &
                              Np, l, d
-                      
+
       REAL, DIMENSION(7)  :: bn_rg2 
       REAL, DIMENSION(6)  :: bn_rg3
       ! Out
@@ -97,45 +97,49 @@ MODULE Mod_drop_growth
 
       !---------------------------- parameter list -----------------------------
       T0      = 293.15                 ! [ K ] , 20C
-      l0      = 6.62 * ((1./10.)**(6.))    ! [ cm ]
+      l0      = 6.62e-6    ! [ cm ]
       p0      = 1013.25                ! [ hPa = mb ] , standard sea level pressure   
       rhov    = p * 100. / ( Rd * T )  ! [ kg * m^-3  ] , density of air
       drho    = rho - rhov             ! [ kg * m^-3  ] , 
                                        ! difference between fluid density of drop and air
 
-      eta0    = 1.818 * ((1./10.)**(4.))     ! [  g * cm^-1 * s^-1 ] , at 20C 
-      eta0    = eta0*0.1               ! [ kg * m^-1 * s^-1  ]
+      eta0    = 1.818e-4     ! [  g * cm^-1 * s^-1 ] , at 20C 
+      ! eta0    = eta0*0.1               ! [ kg * m^-1 * s^-1  ]
 
       ! dynamic viscosity, A short course in cloudphysics 3rd, 102 page
-      eta     = 1.72 * ((1./10)**(5.)) * ( 393 / ( T + 120) ) * (( T / 273 )**(3./2.))
+      eta     = 1.72e-5 * ( 393 / ( T + 120) ) * (( T / 273 )**(3./2.))
 
       ! [ cm ] mean free path of air molcules
       l        = l0 * ( eta / eta0 ) * ( p0 / p ) * (( T / T0 )**(1/2))
-      l        = l * 0.01    ! [ m ]                                         
+      ! l        = l * 0.01    ! [ m ]                                         
 
-      d = 2.*r
-      
+      d = 2.*r*1e+6
+
       ! slip correction factor 
       C_sc = 1. + 2.51 * ( l / d )  
 
+
       !------------------------------ Regime list ------------------------------
-      IF ( d <= 5e-7 ) THEN !------------------------------------------ d <=5e-7
+      ! IF ( d <= 5e-7 ) THEN !------------------------------------------ d <=5e-7
+      IF ( d <= 0.5 ) THEN !------------------------------------------ d <=5e-7
         Vt = 0.
-      ELSE IF ( 5e-7 <= d .and. d < 1.9e-5 ) THEN !-------------------- Regime 1 
+      ! ELSE IF ( 5e-7 <= d .and. d < 1.9e-5 ) THEN !-------------------- Regime 1 
+      ELSE IF ( 0.5 <= d .and. d < 19 ) THEN !-------------------- Regime 1 
         ! [m^-1 * s^-1] = [kg * m^-3] * [m * s^-2] * [kg^-1 * m * s]
         C = ( drho * g ) / ( 18. * eta )
-       
+
         ! [m * s^-1] = [m^-1 * s^-1] * [dimensionless] * [m^2]
-        Vt = C * C_sc * (d**2.)  
-                                            
-      ELSE IF ( 1.9e-5 <= d .and. d < 1.07e-3 ) THEN !------------------ Regime 2 
+        Vt = C * C_sc * ((d*1e-6)**2.)  
+
+      ! ELSE IF ( 1.9e-5 <= d .and. d < 1.07e-3 ) THEN !------------------ Regime 2 
+      ELSE IF ( 19 <= d .and. d < 1.07e+3 ) THEN !------------------ Regime 2 
         ! [m^-3] = [kg * m^-3] * [kg * m^-3]
         !            * [m * s^-2] * [kg^-2 * m^2 * s^2]
-        C = 4. * rho * ( drho ) * g / ( 3. * (eta**2.) )
+        C = 4. * rhov * ( drho ) * g / ( 3. * (eta**2.) )
 
         ! Davies number
         ! [ dimensionless ] = [ m^-3 ] * [ m^3 ] 
-        N_Da = C * (d**3.)  
+        N_Da = C * ((d*1e-6)**3.)  
 
         X_rg = log( N_Da )
 
@@ -159,26 +163,28 @@ MODULE Mod_drop_growth
         ! terminal velocity at regime 2
         ! [ m * s^-1 ]    = [ kg * m^-1 * s^-1 ] 
         !                   * [ dimensionless ] * [ kg^-1 * m^3 ] * [ m^-1 ] 
-        Vt = eta * N_Re / ( rho * d )  
-      ELSE IF ( 1.07e-3 <= d .and. d < 0.007 ) THEN !------------------- Regime 3 
+        Vt = eta * N_Re / ( rhov * d*1e-6 )  
+      ! ELSE IF ( 1.07e-3 <= d .and. d < 0.007 ) THEN !------------------- Regime 3 
+
+      ELSE IF ( 1.07e+3 <= d .and. d < 7e+3 ) THEN !------------------- Regime 3 
         ! approx. surface tension , A short course in cloudphysics 3rd. 85 page
         ! [ N * m^-1 ] = [ kg * m * s^-2 * m^-1 ] = [ kg * s^-2 ]
-        ! sigma = 7.5 * (10.**(-2.)) 
+        ! sigma = 7.5 * (1e-2) 
         ! Yau (1996) - 6.9 problem                                  
-        sigma = (-1.55*1e-4)*T + 0.118   ! Resonable at -20 ~ 20 [K] temperature
+        sigma = (-1.55e-4)*T + 0.118   ! Resonable at -20 ~ 20 [K] temperature
 
         ! [ m^-2 ]     = [ kg * m^-3 ] * [ m * s^-2 ] * [ kg^-1 * s^2 ] 
         C = 4. *  drho  * g  / ( 3. * sigma )           
 
-        ! modified bond number 
+        ! modified bond umber 
         ! [ dimensionless ] = [ m^-2 ] * [ m^2 ]
-        Bo = C * ( d )**2
+        Bo = C * ( d*1e-6 )**2
 
         ! physical property number
         ! [ dimensionless ] = [ kg^3 * s^-6 ] * [ kg^2 * m^-6 ]
         !                     * [ kg^-4 * m^4 * s^4 ] * [ kg^-1 * m^3 ] 
         !                     * [ m^-1 * s^2 ] 
-        Np = (sigma**3) * (rho**2) / ( (eta**4) * drho * g )     
+        Np = (sigma**3) * (rhov**2) / ( (eta**4) * drho * g )     
 
         ! [ dimensionless ] = [ dimensionless ] * [ dimensionless ]                                                               
         X_rg     = log( Bo * (Np**(1./6.)) )                      
@@ -199,15 +205,18 @@ MODULE Mod_drop_growth
         ! terminal velocity at regime 3
         ! [ m * s^-1 ]    = [ kg * m^-1 * s^-1 ] 
         !                   * [ dimensionless ] * [ kg^-1 * m^3 ] * [ m^-1 ]
-        Vt  = ( eta * N_Re ) / ( rho * d ) 
-      ELSE IF ( d >= 0.007 ) THEN !------------------------------------ d >=0.007
-        d0_max = 0.007
-
-        ! sigma  = 7.5 * (10.**(-2.))                                   
+        Vt  = ( eta * N_Re ) / ( rhov * d*1e-6 ) 
+        ! write(*,*) bo, np, x_rg, y_rg, N_Re 
+        ! write(*,*) C, c_sc, eta, vt
+        ! stop
+      ! ELSE IF ( d >= 0.007 ) THEN !------------------------------------ d >=0.007
+      ELSE IF ( d >= 7e+3 ) THEN !------------------------------------ d >=0.007
+        d = 7e+3
+        ! sigma  = 7.5 * (1e-2)                                   
         sigma = (-1.55*1e-4)*T + 0.118   ! Resonable at -20 ~ 20 [K] temperature
         C      = 4. *  drho  * g  / ( 3. * sigma )           
-        Bo     = C * ( d0_max )**2
-        Np     = (sigma**3) * (rho**2) / ( (eta**4) * drho * g )     
+        Bo     = C * ( d*1e-6 )**2
+        Np     = (sigma**3) * (rhov**2) / ( (eta**4) * drho * g )     
 
         bn_rg3    = (/ -0.500015e+1 , 0.523778e+1, -0.204914e+1, &
                           0.475294, -0.542819e-1, 0.238449e-2/)
@@ -222,13 +231,13 @@ MODULE Mod_drop_growth
 
         N_Re = (Np**(1./6.)) * exp(Y_rg)
 
-        Vt  = ( eta * N_Re ) / ( rho * d0_max )     
+        Vt  = ( eta * N_Re ) / ( rhov * d*1e-6 )     
       ENDIF
 
       END SUBROUTINE terminal_velocity!}}}
 
     !== !== !==
-    SUBROUTINE ventilation(T, P, r, Vf) !{{{
+   SUBROUTINE ventilation(T, P, r, Vf) !{{{
 
       IMPLICIT NONE
       ! In
@@ -250,17 +259,15 @@ MODULE Mod_drop_growth
         call terminal_velocity(T, P, r(irr), Vt(irr))   ! Beard (1976)
       END DO
 
-      write(*,*) vt
-      write(*,*) "==========" 
-      write(*,*) r 
+      ! write(*,*) shape(vt)
+      ! write(*,*) "==========" 
+      ! write(*,*) r 
       ! open(unit = 111, file = 'test.txt', status = "unknown", &
       !       access="direct", form="unformatted", recl=100*4) 
       !
       ! write(unit = 111, rec=1) vt
-      ! write(unit = 111, rec=2) r
-
-
-      stop
+      !
+      ! stop
       ! dynamic viscosity of air (See Yau (1996) 102-103p)
       eta = 1.72e-5 * ( 393./(T+120.) ) * ( T/273. )**(3./2.)    ! approximate formula
       ! mu = 1.717e-5          ! [kg m-1 s-1] (at 273 [K])
