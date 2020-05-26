67e8383612562c3a049f511fc696b90b3e21069c
hanjihye9490 <gihh1131@smail.kongju.ac.kr>
2020-05-26 16:05:35 +0900

    modify reassin and mass

--- src/phys/Mod_phys_driver.f90
+++ src/phys/Mod_phys_driver.f90
@@ -11,13 +11,16 @@ MODULE Mod_phys_driver
   SUBROUTINE Sub_phys_driver
 
   IMPLICIT NONE
+    INTEGER :: i
 
     write(*,*) "==============================================================="
     write(*,*) "for checking conservation after condensation and redistribution"
     write(*,*) " before "
     write(*,*) " num of bot layer at 1st time  =", sum(drop%num(1,:))
     write(*,*) " num of top layer at 1st time  =", sum(drop%num(100,:))
-    DO iz = 1, nz
+
+    !DO iz = 1, nz
+    DO iz = 1, 1 
 
       ! condensation and evaporation
       CALL compute_dmb_dt                       &
@@ -30,6 +33,16 @@ MODULE Mod_phys_driver
       drop%m(iz,:)  = drop%m(iz,:)  + drop%dm_dt(iz,:)*dt
       drop%mb(iz,:) = drop%mb(iz,:) + drop%dmb_dt(iz,:)*dt
 
+      !! when the mass value have minus in the evaporation process, 
+      !! physically this makes no sense
+      !! 2020.05.26 by han start
+      DO i = 1, SIZE(drop%mb(iz,:)) 
+        IF (drop%mb(iz,i) < 0.) drop%mb(iz,i) = 0
+        IF (i == SIZE(drop%mb(iz,:))) EXIT
+        IF (drop%m(iz,i) < 0.) drop%m(iz,i) = 0
+      ENDDO
+      !! 2020.05.26 by han end  
+ 
       CALL compute_redist                           &
            (                                        &
             drop%ref_m(:),  drop%m(iz,:),           &
@@ -40,11 +53,13 @@ MODULE Mod_phys_driver
 
       drop%num(iz,:) = drop%next_num(iz,:)
 
-      ! drop%dqv    = -1 * (total_m(it) - total_m(it-1))
-      ! drop%dTemp  = -1 * (L* drop%dqv)/(rho*Cp) 
+ !      total_m(it) = SUM(drop%next_num(iz,:)*drop%m(iz,:))
+ !      drop%dqv    = -1 * (total_m(it) - total_m(it-1))/rho
+ !      drop%dTemp  = -1 * (L* drop%dqv)/(rho*Cp) 
 
-      ! temp%next_dz=temp%dz !- dl/dt
-      ! q%next_dz=q%dz !- dRH/dt
+       !temp%next_dz=temp%dz !- dl/dt
+!       q%next_dz=q%dz + drop%dqv
+print*, 'total mass :', SUM(drop%next_num(iz,:)*drop%m(iz,:))
       ! collision./com
       ! CALL compute_collision
     ENDDO
